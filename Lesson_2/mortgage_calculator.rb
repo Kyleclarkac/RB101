952920f9ec0fee8f28693bed8d7841ff4b59b77f
Kyle Clark <kyleclarkac@gmail.com>
2021-02-21 12:32:32 -0500

    final

--- Lesson_2/mortgage_calculator.rb
+++ Lesson_2/mortgage_calculator.rb
@@ -1,19 +1,26 @@
+def valid_float(string)
+
+  return v_float
+end
+
 # This program will take in three inputs from a user and display mortgage data
 
 puts "Hello! Welcome to the Mortgage Calculator. Let's see what you'll be paying"
 puts
 #Get inputs from user for Loan amount, apr, and duration.
 
-puts "Please enter the total loan amount in dollars"
-amount = gets.chomp.to_f
+loop do
+  puts "Please enter the total loan amount in dollars"
+  amount = gets.chomp.to_f
+  break if amount.to_s.to_f
 
 puts "Please enter the APR as a percent"
-apr = gets.chomp.to_f/(12*100)
+monthly_interest = gets.chomp.to_f/(12*100)
 
 puts "Please enter the load duration in years"
 duration = gets.chomp.to_f*12
 
-monthly_payment = amount * (apr / (1 - (1 + apr)**(-duration)))
+monthly_payment = amount * (monthly_interest / (1 - (1 + monthly_interest)**(-duration)))
 
 
 puts "Your month payment will be #{monthly_payment}"
